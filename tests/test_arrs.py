91c4fb448cd6deca4e9ca5b3f8e793e89293ce49
AlexandrKruglov <krulalex84@gmail.com>
2024-01-28 13:27:01 +0300

    corrected test

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -9,3 +9,11 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 4]) == [1, 2, 3, 4]
+    assert arrs.my_slice([1, 2, 3], None, 2) == [1, 2]
+    assert arrs.my_slice([1, 2, 3, 4], -2) == [3, 4]
+    assert arrs.my_slice([1, 2, 3, 4, 5], -3, -1) == [3, 4]
+    assert arrs.my_slice([]) == []
+
+
+
